2c9b81d76012120da2030bc17be86701f5414551
Danielle Sikich (Intel) <danielle.sikich@intel.com>
2020-01-27 14:24:04 -0800

    remove post and libcurl from build

--- src/main.c
+++ src/main.c
@@ -2,7 +2,6 @@
 
 DEFINE_OBJECT(Options, options);
 
-extern int post_main(int argc, char** argv);
 extern int sse_main(int argc, char** argv);
 
 /*
@@ -10,9 +9,5 @@ extern int sse_main(int argc, char** argv);
  */
 int main(int argc, char** argv) {
   options.arg0 = *argv;
-
-  if(streeq(*argv, "post"))
-    return post_main(argc, argv);
-
   return sse_main(argc, argv);
 }
